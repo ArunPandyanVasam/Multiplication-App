e9c170f138db73318b5c0f5bf17f4822ef02c051
ArunPandyanVasam <you@example.com>
2023-08-23 18:48:14 -0400

    Done

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const num1 = parseInt(Math.random() * 10);
-const num2 = parseInt(Math.random() * 10);
-
 const questionEl = document.getElementById("question"); 
 const inputEl = document.getElementById("input");
 const formEl = document.getElementById("form"); 
-
-questionEl.innerText = `What is ${num1} multiplied by ${num2}?`; 
-const correctAns = num1 * num2;
+const scoreEl = document.getElementById("score");
 
 let score = 0;
 
+function generateQuestion() {
+    const num1 = parseInt(Math.random() * 10);
+    const num2 = parseInt(Math.random() * 10);
+    questionEl.innerText = `What is ${num1} multiplied by ${num2}?`;
+    return num1 * num2; // Return the correct answer
+}
+
+let correctAns = generateQuestion();
+
 formEl.addEventListener("submit", (event) => { 
-    //event.preventDefault(); // Prevent the form from submitting and refreshing the page
+    event.preventDefault();
     const userAns = parseInt(inputEl.value);
     if (userAns === correctAns) { 
         score++;
-        console.log(score);
     } else {
         score--;
-        console.log(score);
     }
+    scoreEl.innerText = `Score = ${score}`;
+    
+    correctAns = generateQuestion(); // Generate a new question and update correct answer
+    inputEl.value = ''; // Clear the input field
 });
 
-const scoreE1 = document.getElementById("score");
-scoreE1.innerText = `Score = ${score}`;
+scoreEl.innerText = `Score = ${score}`;
